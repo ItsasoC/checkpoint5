be9a44baa9414cf4676c7e0edc9498dd234992cf
Itsaso Carmona <itsaso.carmona@gmail.com>
2024-05-22 09:55:29 +0200

    Ejercicio practico terminado y el teorico sin terminar

---
+++ Ejercicio practico.py
@@ -0,0 +1,26 @@
+# Cree un bucle For de Python.
+for town in ["Paris", "Albacete","Oñati"] :
+    print(f'El participante es de {town}')
+
+# Cree una función de Python llamada suma que tome 3 argumentos y 
+#devuelva la suma de los 3.
+def suma(arg1,arg2,arg3):
+    return arg1+arg2+arg3
+
+# Cree una función lambda con la misma funcionalidad que la función de suma 
+# que acaba de crear.
+suma_total = lambda arg1,arg2,arg3 : arg1+arg2+arg3
+print(suma_total(1,1,1))
+
+# Utilizando la siguiente lista y variable, determine si el valor de la variable 
+#coincide o no con un valor de la lista. 
+#*Sugerencia, si es necesario, utilice un bucle for in y el operador in.
+
+nombre = 'Enrique'
+lista_nombre = ['Jessica', 'Paul', 'George', 'Henry', 'Adán']
+
+for nomb in lista_nombre :
+    if nombre == nomb :
+        print(f"El nombre {nomb} aparece en la lista")
+    else:
+        print(f'El nombre no es {nomb}')
